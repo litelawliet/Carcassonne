3388fa6c0fe4f89cf773b30e56eb67250cd662d4
Warchange <31731763+Warchange@users.noreply.github.com>
2017-11-09 16:27:53 +0100

    Update Board.java

--- src/Carcassonne/Board.java
+++ src/Carcassonne/Board.java
@@ -1,5 +1,6 @@
 package Carcassonne;
 
+import java.awt.*;
 import java.util.ArrayList;
 
 /**
@@ -14,11 +15,15 @@ public class Board {
     // "Matrice" contenant les cartes paysage
     private ArrayList<ArrayList<Card>> tiles;
 
+    // Carte centrale, celle à partir de laquelle on dessine
+    private Card middleCard;
+
     /**
      * Constructeur.
      */
     public Board() {
         this.tiles = new ArrayList<>();
+        this.middleCard = null;
     }
 
     public ArrayList<ArrayList<Card>> getTiles() {
@@ -40,10 +45,12 @@ public class Board {
      * Ajoute une carte paysage au jeu.
      * @param c la carte à ajouter
      */
-    public void addTile(Card c, int row, int col) throws TileOutOfRangeException {
+    public void addTile(Card c, int row, int col) throws TileOutOfRangeException, TileHasNoNeighborException {
         if(c == null) return;
 
-        if(row < 0 || col < 0) throw new TileOutOfRangeException();
+        if(row < 0 || col < 0) throw new TileOutOfRangeException(); // valeurs négatives
+
+        if(!this.hasNeighbors(row, col)) throw new TileHasNoNeighborException(); // la carte doit avoir une voisine
 
         // Row = numéro liste dans la grosse liste
         // Col = numéro carte dans la liste row
@@ -79,8 +86,9 @@ public class Board {
      * Pour ajouter la première carte paysage rapidement
      * @param c la carte en question
      */
-    public void addFirstTile(Card c) throws TileOutOfRangeException {
+    public void addFirstTile(Card c) throws TileOutOfRangeException, TileHasNoNeighborException {
         this.addTile(c, 0, 0);
+        this.middleCard = c;
     }
 
     /**
@@ -122,7 +130,64 @@ public class Board {
         return getNumTiles() == 0;
     }
 
+    /**
+     * Récupére la carte en x, y
+     * @param x x
+     * @param y y
+     * @return la carte en x, y
+     */
+    public Card getTileAt(int x, int y){
+        if(x > this.tiles.size() || y > this.tiles.get(x).size()) return null;
+        return this.tiles.get(x).get(y);
+    }
+
+    /**
+     * Vérifie que la carte a une voisine
+     * @param row ligne
+     * @param col colonne
+     * @return true quand la carte a une voisine
+     */
+    public boolean hasNeighbors(int row, int col){
+        if(middleCard == null) return true;// ne fait pas le check pour la première carte
+
+        // Si on insère une carte
+        if(row >= 0 && col >= 0 && row < tiles.size() && col < tiles.get(row).size()){
+            if(tiles.get(row).get(col) != null){
+                row--;
+                col--;
+            }
+        }
+
+        // le check
+        try {
+            if(row >= 1){
+                if(tiles.get(row-1).get(col) != null) return true;
+            }
+        } catch (Exception e){/*nothing*/}
+
+        try {
+            if(row < tiles.size()-1){
+                if(tiles.get(row+1).get(col) != null) return true;
+            }
+        } catch (Exception e){/*nothing*/}
+
+        try {
+            if(col >= 1){
+                if(tiles.get(row).get(col-1) != null) return true;
+            }
+        } catch (Exception e){/*nothing*/}
+
+        try {
+            if(col < tiles.get(row).size()-1){
+                if(tiles.get(row).get(col+1) != null) return true;
+            }
+        } catch (Exception e){/*nothing*/}
+
+        return false;
+    }
+
+
+
     // TODO
     // diverses précisions : pour récupérer la position de la carte (afin d'ajouter des cartes) : indexOf
-    // il faut un test comme quoi la carte est compatible avec sa voisine
 }
